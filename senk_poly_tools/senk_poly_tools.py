857b6bd4e768f055b30502c29973df2e010c8d18
Gyurkó, M. Dávid, 'dualon <gyurko.david@e-arc.hu>
2015-03-16 23:49:53 +0100

    Restructured, cleaned code. More verbose output. CO2 channel findExtrema hwl=2000.

--- senk_poly_tools/senk_poly_tools.py
+++ senk_poly_tools/senk_poly_tools.py
@@ -25,10 +25,17 @@ from edf_container import EdfContainer # senk_poly_tools.
 
 class SenkPolyTools(object):
 	
-	annot_label = 'EDF Annotations'
+	annot_label 		= 'EDF Annotations'
+	results_base_path 	= os.path.abspath(os.path.join(
+							os.path.dirname(__file__),
+							'..',
+							'results')
+							)
 	
 	def createEdfContainer(self, file_name):
 		""" Open an EDF(+) file and return an empty EdfContainer object. """
+		
+		print("Creating EDF(+) container... ", end="")
 		
 		abs_path = os.path.abspath(os.path.join(
 			os.path.dirname(__file__),
@@ -42,6 +49,8 @@ class SenkPolyTools(object):
 		
 		e = EdfContainer(abs_path)
 		e.file_obj = open(abs_path, 'rb')
+		
+		print("OK")
 		
 		return e
 	
@@ -63,6 +72,8 @@ class SenkPolyTools(object):
 				see SenkPolyTools.createEdfContainer()
 			returns EdfContainer with loaded headers
 		"""
+		
+		print("Loading EDF(+) headers... ", end = "")
 		
 		if not isinstance(e, EdfContainer):
 			raise Exception("The provided container is not an EdfContainer! Use the container returned by SenkPolyTools.openEdf().")
@@ -122,6 +133,8 @@ class SenkPolyTools(object):
 		if f.tell() != e.num_of_bytes_in_header:
 			raise ValueError("The number of bytes in the header does not match the file object cursor. Header length mismatch during reading?");
 		
+		print("OK")
+		
 		return e
 	
 	
@@ -133,6 +146,8 @@ class SenkPolyTools(object):
 				see SenkPolyTools.loadEdfHeaders()
 			returns EdfContainer with loaded data
 		"""
+		
+		print("Loading EDF(+) data... ", end = "")
 		
 		if e.file_obj is None:
 			raise AttributeError("EdfContainer.file_obj is missing, use SenkPolyTools.createEdfContainer() to create a file stream.")
@@ -192,6 +207,8 @@ class SenkPolyTools(object):
 			data[chn_idx] = np.array(chn_data)
 		e.data = np.array(data) # e.data is a numpy 2D matrix
 		del data
+		
+		print("OK")
 		
 		return e
 	
@@ -291,7 +308,8 @@ class SenkPolyTools(object):
 		x_len = x.shape[0]
 
 		if x_len < hwl:
-			raise Exception("SenkPolyTools.findExtrema: data length < hwl")
+			msg = "SenkPolyTools.findExtrema: data length ({}) < hwl ({})".format(x_len, hwl)
+			raise Exception(msg)
 		
 		minima = []
 		maxima = []
@@ -388,8 +406,251 @@ class SenkPolyTools(object):
 			interpolated_y.append( (sx, iy) )
 		
 		return interpolated_y
+
+
+	def exportAnnotations(self, edfc):
+		print("Annotations... ", end='')
 		
+		annot_fname = os.path.join(self.results_base_path, "{}_annotations.txt".format(edfc.file_basename))
+		
+		with open(annot_fname, 'w', newline='') as annfp:
+			csvw = csv.writer(annfp, dialect='excel', delimiter=';')
+			
+			csvw.writerow(['Onset', 'Duration', 'Annotation(s)'])
+			for ann in edfc.annotations:
+				if ann['annotation']:
+					csvw.writerow([
+						ann['onset'],
+						ann['duration'],
+						ann['annotation']
+					])
+		print("OK")
 	
+	
+	def processSenkEdf(self, edfc):
+		""" Process an EDF file specific to the SE Neurology Clinic.
+		
+			This is a high level method which takes an EdfContainer object and processes and exports the ECG, TCD, ABP and CO2 channels.
+			The output is a bunch of text files in the /results directory.
+		"""
+		
+		for chn_i, chn_data in enumerate(edfc.data):
+			chn_n = edfc.data_labels[chn_i]
+			
+			if 'EKG' in chn_n or 'ECG' in chn_n:
+				spt.exportSenkEcg(edfc, chn_i)
+			
+			elif 'TCD' in chn_n:
+				spt.exportSenkTcd(edfc, chn_i)
+				
+			elif 'Tonometry' in chn_n: # ABP
+				spt.exportSenkAbp(edfc, chn_i)
+			
+			elif 'CO2' in chn_n:
+				spt.exportSenkCo2(edfc, chn_i)
+			
+			else:
+				print("Channel '{}'... skipped".format(chn_n))
+	
+	
+	def exportSenkEcg(self, edfc, chn_num):
+		chn_n = edfc.data_labels[chn_num]
+		data = edfc.data[chn_num]
+		
+		print("Channel '{}'... ".format(chn_n), end="")
+			
+		sm_data = spt.smoothByAvg(data, 8)
+		_, ecg_maxima = spt.findExtrema(sm_data)
+		ecg_max_ind, __ = zip(*ecg_maxima)
+		ecg_dists = spt.indexDists(ecg_max_ind)
+		
+		ecg_freq = edfc.sample_freq[chn_num]
+		hr = [60/(dist/ecg_freq) for dist in ecg_dists]
+		times = [curr_ecg_idx/ecg_freq for curr_ecg_idx in ecg_max_ind]
+		times.insert(0, 0.0)
+		
+		fname = os.path.join(self.results_base_path, "{}_{}_heart_rate.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
+		with open(fname, "w", newline='') as fp:
+			csvw = csv.writer(fp, dialect='excel', delimiter=';')
+			
+			csvw.writerow(['Time (sec)', 'Heart Rate'])
+			for ecg_t, ecg_hr in zip(times, hr):
+				csvw.writerow([ecg_t, ecg_hr])
+		
+		print("OK")
+	
+	
+	def exportSenkTcd(self, edfc, chn_num):
+		chn_n = edfc.data_labels[chn_num]
+		data = edfc.data[chn_num]
+		
+		print("Channel '{}'... ".format(chn_n), end="")
+		
+		sm_data = spt.smoothByAvg(data)
+		tcd_mins, tcd_maxes = spt.findExtrema(sm_data)
+		
+		sm_data_len = len(sm_data)
+		tcd_sampling = int(edfc.sample_freq[chn_num]*0.5) # 500 Hz * 0.5
+		interp_x = [ix for ix in range(0, sm_data_len, tcd_sampling)]
+		
+		tcd_interp_mins = spt.interpolate(tcd_mins, interp_x)
+		tcd_interp_maxes = spt.interpolate(tcd_maxes, interp_x)
+		
+		# 5 seconds moving average on interpolated minima
+		_, tcd_imins_vals = zip(*tcd_interp_mins)
+		tcd_imins_vals = np.array(tcd_imins_vals)
+		tcd_imins_avg = spt.smoothByAvg(tcd_imins_vals, 10)
+		
+		# 5 seconds moving average on interpolated maxima
+		_, tcd_imaxes_vals = zip(*tcd_interp_maxes)
+		tcd_imaxes_vals = np.array(tcd_imaxes_vals)
+		tcd_immaxes_avg = spt.smoothByAvg(tcd_imaxes_vals, 10)
+		
+		fname = os.path.join(self.results_base_path, "{}_{}_0.5hz.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
+		with open(fname, "w", newline='') as fp:
+			csvw = csv.writer(fp, dialect='excel', delimiter=';')
+			csvw.writerow([
+				"Time (sec)",
+				"TCD Maxima",
+				"TCD Minima",
+				"(Max + 2*Min)/3",
+				"TCD Maxima 5sec Moving Avg",
+				"TCD Minima 5sec Moving Avg",
+				"Moving Avg (Max + 2*Min)/3",
+				"Resistance Index of Moving Avg"
+			])
+			
+			for tcd_mn, tcd_mx, tcd_imn_a, tcd_imx_a in zip(tcd_interp_mins, tcd_interp_maxes, tcd_imins_avg, tcd_immaxes_avg):
+				csvw.writerow([
+					tcd_mn[0]/edfc.sample_freq[chn_num],
+					tcd_mx[1],
+					tcd_mn[1],
+					(tcd_mx[1] + 2*tcd_mn[1])/3,
+					tcd_imx_a,
+					tcd_imn_a,
+					(tcd_imx_a + 2*tcd_imn_a)/3,
+					(tcd_imx_a - tcd_imn_a)/tcd_imx_a
+				])
+		
+		print("OK")
+	
+	
+	def exportSenkAbp(self, edfc, chn_num):
+		chn_n = edfc.data_labels[chn_num]
+		data = edfc.data[chn_num]
+		
+		print("Channel '{}'... ".format(chn_n), end="")
+		
+		sm_data = spt.smoothByAvg(data, 8)
+		abp_mins, abp_maxes = spt.findExtrema(sm_data)
+		
+		# sampling settings
+		abp_freq = edfc.sample_freq[chn_num]
+		sm_data_len = len(sm_data)
+		abp_sampling = int(abp_freq*0.5) # 500 Hz * 0.5
+		interp_x = [ix for ix in range(0, sm_data_len, abp_sampling)]
+		
+		# heart rate from ABP for every half second, because ECG channel is often missing...
+		abp_max_ind, __ = zip(*abp_maxes)
+		abp_max_dists = spt.indexDists(abp_max_ind)
+		hr = [60/(dist/abp_freq) for dist in abp_max_dists]
+		
+		abp_hr_interp = spt.interpolate(zip(abp_max_ind, hr), interp_x)
+		
+		# Half Hertz sampling of interpolated minima and maxima
+		abp_interp_mins = spt.interpolate(abp_mins, interp_x)
+		abp_interp_maxes = spt.interpolate(abp_maxes, interp_x)
+		
+		# 5 seconds moving average on interpolated minima
+		_, abp_imins_vals = zip(*abp_interp_mins)
+		abp_imins_vals = np.array(abp_imins_vals)
+		abp_imins_avg = spt.smoothByAvg(abp_imins_vals, 10)
+		
+		# 5 seconds moving average on interpolated maxima
+		_, abp_imaxes_vals = zip(*abp_interp_maxes)
+		abp_imaxes_vals = np.array(abp_imaxes_vals)
+		abp_imaxes_avg = spt.smoothByAvg(abp_imaxes_vals, 10)
+		
+		fname = os.path.join(self.results_base_path, "{}_{}_0.5hz.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
+		
+		with open(fname, "w", newline='') as fp:
+			csvw = csv.writer(fp, dialect='excel', delimiter=';')
+			csvw.writerow([
+				"Time (sec)",
+				"ABP Systole",
+				"ABP Diastole",
+				"(Syst + 2*Diast)/3",
+				"ABP Systole 5sec Moving Avg",
+				"ABP Diastole 5sec Moving Avg",
+				"Moving Avg (Syst + 2*Diast)/3",
+				"Heart Rate (Derived)"
+			])
+			
+			for abp_mn, abp_mx, abp_imn_a, abp_imx_a, abp_hr in zip(abp_interp_mins, abp_interp_maxes, abp_imins_avg, abp_imaxes_avg, abp_hr_interp):
+				csvw.writerow([
+					abp_mn[0]/abp_freq,
+					abp_mx[1],
+					abp_mn[1],
+					(abp_mx[1] + 2*abp_mn[1])/3,
+					abp_imx_a,
+					abp_imn_a,
+					(abp_imx_a + 2*abp_imn_a)/3,
+					abp_hr[1]
+				])
+		
+		print("OK")
+	
+	
+	def exportSenkCo2(self, edfc, chn_num):
+		chn_n = edfc.data_labels[chn_num]
+		data = edfc.data[chn_num]
+		
+		print("Channel '{}'... ".format(chn_n), end="")
+		
+		sm_data = spt.smoothByAvg(data, 8)
+		
+		sm_data_len = len(sm_data)
+		co2_freq = edfc.sample_freq[chn_num]
+		co2_sampling = int(co2_freq*0.5) # 500 Hz * 0.5
+		interp_x = [ix for ix in range(0, sm_data_len, co2_sampling)]
+		
+		# respiratory peaks
+		_, co2_maxima = spt.findExtrema(sm_data, hwl=2000)
+		
+		# respiratory rate
+		co2_max_ind, __ = zip(*co2_maxima)
+		co2_dists = spt.indexDists(co2_max_ind)
+		rr = [60/(dist/co2_freq) for dist in co2_dists]
+		
+		co2_rr_interp = spt.interpolate(zip(co2_max_ind, rr), interp_x)
+		
+		# .5 Hz sampling of respiratory peaks
+		co2_interp_maxes = spt.interpolate(co2_maxima, interp_x)
+		
+		# 5 seconds moving average on interpolated maxima
+		_, co2_imaxes_vals = zip(*co2_interp_maxes)
+		co2_imaxes_vals = np.array(co2_imaxes_vals)
+		co2_immaxes_avg = spt.smoothByAvg(co2_imaxes_vals, 10)
+		
+		fname = os.path.join(self.results_base_path, "{}_{}_0.5hz.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
+		with open(fname, "w", newline='') as fp:
+			csvw = csv.writer(fp, dialect='excel', delimiter=';')
+			csvw.writerow([
+				"Time (sec)",
+				"CO2 Maxima",
+				"CO2 Maxima 5sec Moving Avg",
+				"Respiratory Rate"
+			])
+			
+			for co2_mx, co2_imx_a, curr_rr in zip(co2_interp_maxes, co2_immaxes_avg, co2_rr_interp):
+				csvw.writerow([
+					(co2_mx[0]/co2_freq) - 7, # respiratory channel lag: 7 seconds
+					co2_mx[1],
+					co2_imx_a,
+					curr_rr[1]
+				])
+		
+		print("OK")
 	
 
 
@@ -404,188 +665,5 @@ if __name__ == '__main__':
 	spt = SenkPolyTools()
 	edfc = spt.loadEdf(args.edf)
 	
-	results_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'results'))
-	
-	print("Annotations... ", end='')
-	annot_fname = os.path.join(results_base_path, "{}_annotations.txt".format(edfc.file_basename))
-	with open(annot_fname, 'w', newline='') as annfp:
-		csvw = csv.writer(annfp, dialect='excel', delimiter=';')
-		
-		csvw.writerow(['Onset', 'Duration', 'Annotation(s)'])
-		for ann in edfc.annotations:
-			if ann['annotation']:
-				csvw.writerow([
-					ann['onset'],
-					ann['duration'],
-					ann['annotation']
-				])
-	print("OK")
-	
-	for chn_i, chn_data in enumerate(edfc.data):
-		chn_n = edfc.data_labels[chn_i]
-		chn_data = edfc.data[chn_i]
-		print("Channel '{}'... ".format(chn_n), end="")
-		
-		if 'EKG' in chn_n or 'ECG' in chn_n:
-			ecg_freq = edfc.sample_freq[chn_i]
-			
-			sm_data = spt.smoothByAvg(chn_data, 8)
-			_, ecg_maxima = spt.findExtrema(sm_data)
-			ecg_max_ind, __ = zip(*ecg_maxima)
-			ecg_dists = spt.indexDists(ecg_max_ind)
-			hr = [60/(dist/ecg_freq) for dist in ecg_dists]
-			times = [curr_ecg_idx/ecg_freq for curr_ecg_idx in ecg_max_ind]
-			times.insert(0, 0.0)
-			
-			fname = os.path.join(results_base_path, "{}_{}_heart_rate.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
-			with open(fname, "w", newline='') as fp:
-				csvw = csv.writer(fp, dialect='excel', delimiter=';')
-				
-				csvw.writerow(['Time (sec)', 'Heart Rate'])
-				for ecg_t, ecg_hr in zip(times, hr):
-					csvw.writerow([ecg_t, ecg_hr])
-			
-			print("OK")
-		
-		elif 'TCD' in chn_n:
-			sm_data = spt.smoothByAvg(chn_data)
-			tcd_mins, tcd_maxes = spt.findExtrema(sm_data)
-			
-			sm_data_len = len(sm_data)
-			tcd_sampling = int(edfc.sample_freq[chn_i]*0.5) # 500 Hz * 0.5
-			interp_x = [ix for ix in range(0, sm_data_len, tcd_sampling)]
-			
-			tcd_interp_mins = spt.interpolate(tcd_mins, interp_x)
-			tcd_interp_maxes = spt.interpolate(tcd_maxes, interp_x)
-			
-			# 5 seconds moving average on interpolated minima
-			_, tcd_imins_vals = zip(*tcd_interp_mins)
-			tcd_imins_vals = np.array(tcd_imins_vals)
-			tcd_imins_avg = spt.smoothByAvg(tcd_imins_vals, 10)
-			
-			# 5 seconds moving average on interpolated maxima
-			_, tcd_imaxes_vals = zip(*tcd_interp_maxes)
-			tcd_imaxes_vals = np.array(tcd_imaxes_vals)
-			tcd_immaxes_avg = spt.smoothByAvg(tcd_imaxes_vals, 10)
-			
-			fname = os.path.join(results_base_path, "{}_{}_0.5hz.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
-			with open(fname, "w", newline='') as fp:
-				csvw = csv.writer(fp, dialect='excel', delimiter=';')
-				csvw.writerow(["Time (sec)", "TCD Maxima", "TCD Minima", "(Max + 2*Min)/3", "TCD Maxima 5sec Moving Avg", "TCD Minima 5sec Moving Avg", "Moving Avg (Max + 2*Min)/3", "Resistance Index of Moving Avg"])
-				
-				for tcd_mn, tcd_mx, tcd_imn_a, tcd_imx_a in zip(tcd_interp_mins, tcd_interp_maxes, tcd_imins_avg, tcd_immaxes_avg):
-					csvw.writerow([
-						tcd_mn[0]/edfc.sample_freq[chn_i],
-						tcd_mx[1],
-						tcd_mn[1],
-						(tcd_mx[1] + 2*tcd_mn[1])/3,
-						tcd_imx_a,
-						tcd_imn_a,
-						(tcd_imx_a + 2*tcd_imn_a)/3,
-						(tcd_imx_a - tcd_imn_a)/tcd_imx_a
-					])
-			
-			print("OK")
-			
-		elif 'Tonometry' in chn_n: # ABP
-			sm_data = spt.smoothByAvg(chn_data, 8)
-			abp_mins, abp_maxes = spt.findExtrema(sm_data)
-			
-			# sampling settings
-			abp_freq = edfc.sample_freq[chn_i]
-			sm_data_len = len(sm_data)
-			abp_sampling = int(abp_freq*0.5) # 500 Hz * 0.5
-			interp_x = [ix for ix in range(0, sm_data_len, abp_sampling)]
-			
-			# heart rate from ABP for every half second, because ECG channel is often missing...
-			abp_max_ind, __ = zip(*abp_maxes)
-			abp_max_dists = spt.indexDists(abp_max_ind)
-			hr = [60/(dist/abp_freq) for dist in abp_max_dists]
-			
-			abp_hr_interp = spt.interpolate(zip(abp_max_ind, hr), interp_x)
-			
-			# Half Hertz sampling of interpolated minima and maxima
-			abp_interp_mins = spt.interpolate(abp_mins, interp_x)
-			abp_interp_maxes = spt.interpolate(abp_maxes, interp_x)
-			
-			# 5 seconds moving average on interpolated minima
-			_, abp_imins_vals = zip(*abp_interp_mins)
-			abp_imins_vals = np.array(abp_imins_vals)
-			abp_imins_avg = spt.smoothByAvg(abp_imins_vals, 10)
-			
-			# 5 seconds moving average on interpolated maxima
-			_, abp_imaxes_vals = zip(*abp_interp_maxes)
-			abp_imaxes_vals = np.array(abp_imaxes_vals)
-			abp_imaxes_avg = spt.smoothByAvg(abp_imaxes_vals, 10)
-			
-			fname = os.path.join(results_base_path, "{}_{}_0.5hz.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
-			
-			with open(fname, "w", newline='') as fp:
-				csvw = csv.writer(fp, dialect='excel', delimiter=';')
-				csvw.writerow(["Time (sec)", "ABP Systole", "ABP Diastole", "(Syst + 2*Diast)/3", "ABP Systole 5sec Moving Avg", "ABP Diastole 5sec Moving Avg", "Moving Avg (Syst + 2*Diast)/3", "Heart Rate (Derived)"])
-				
-				for abp_mn, abp_mx, abp_imn_a, abp_imx_a, abp_hr in zip(abp_interp_mins, abp_interp_maxes, abp_imins_avg, abp_imaxes_avg, abp_hr_interp):
-					csvw.writerow([
-						abp_mn[0]/abp_freq,
-						abp_mx[1],
-						abp_mn[1],
-						(abp_mx[1] + 2*abp_mn[1])/3,
-						abp_imx_a,
-						abp_imn_a,
-						(abp_imx_a + 2*abp_imn_a)/3,
-						abp_hr[1]
-					])
-			
-			print("OK")
-		
-		elif 'CO2' in chn_n:
-			sm_data = spt.smoothByAvg(chn_data, 8)
-			
-			sm_data_len = len(sm_data)
-			co2_freq = edfc.sample_freq[chn_i]
-			co2_sampling = int(co2_freq*0.5) # 500 Hz * 0.5
-			interp_x = [ix for ix in range(0, sm_data_len, co2_sampling)]
-			
-			# respiratory peaks
-			_, co2_maxima = spt.findExtrema(sm_data) # window: 150
-			
-			# respiratory rate
-			co2_max_ind, __ = zip(*co2_maxima)
-			co2_dists = spt.indexDists(co2_max_ind)
-			rr = [60/(dist/co2_freq) for dist in co2_dists]
-			
-			co2_rr_interp = spt.interpolate(zip(co2_max_ind, rr), interp_x)
-			
-			#import matplotlib.pyplot as plt
-			#
-			#f,ax = plt.subplots(1,1,figsize=(80,8))
-			#ax.plot(sm_data[0:50000])
-			#ax.vlines([idx for idx, _ in co2_maxima[0:21]], -150.0, 150.0, color='#ED9A00', alpha=0.5)
-			#plt.savefig(os.path.join('results', '{}.png'.format(edfc.file_basename)))
-			#plt.close()
-			
-			# .5 Hz sampling of respiratory peaks
-			co2_interp_maxes = spt.interpolate(co2_maxima, interp_x)
-			
-			# 5 seconds moving average on interpolated maxima
-			_, co2_imaxes_vals = zip(*co2_interp_maxes)
-			co2_imaxes_vals = np.array(co2_imaxes_vals)
-			co2_immaxes_avg = spt.smoothByAvg(co2_imaxes_vals, 10)
-			
-			fname = os.path.join(results_base_path, "{}_{}_0.5hz.txt".format(edfc.file_basename, chn_n.replace(' ', '')))
-			with open(fname, "w", newline='') as fp:
-				csvw = csv.writer(fp, dialect='excel', delimiter=';')
-				csvw.writerow(["Time (sec)", "CO2 Maxima", "CO2 Maxima 5sec Moving Avg", "Respiratory Rate"])
-				
-				for co2_mx, co2_imx_a, curr_rr in zip(co2_interp_maxes, co2_immaxes_avg, co2_rr_interp):
-					csvw.writerow([
-						(co2_mx[0]/co2_freq) - 7, # respiratory channel lag: 7 seconds
-						co2_mx[1],
-						co2_imx_a,
-						curr_rr[1]
-					])
-			
-			print("OK")
-		
-		else:
-			print("skipped")
+	spt.exportAnnotations(edfc)
+	spt.processSenkEdf(edfc)
